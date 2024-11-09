26db39b931061f292308ef9647b7bf7f0c5cbed9
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-11-09 22:21:54 +0800

    Update synch.sql

--- synch.sql
+++ synch.sql
@@ -1,15 +1,18 @@
-USE ccinfom_database_app;
+CREATE DATABASE dbapp;
+
+USE dbapp;
 
 CREATE TABLE Programs (
-    program_id INT PRIMARY KEY,
+    program_id INT NOT NULL,
     program_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(50),
     degree_level VARCHAR(20),
-    total_units INT
+    total_units INT,
+    CONSTRAINT programs_pk PRIMARY KEY (program_id)
 );
 
 CREATE TABLE Instructor (
-    inst_id INT PRIMARY KEY,
+    inst_id INT NOT NULL,
     inst_first_name VARCHAR(50) NOT NULL,
     inst_last_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(20),
@@ -18,55 +21,60 @@ CREATE TABLE Instructor (
     office_hours VARCHAR(100),
     hire_date DATE,
     employment_status VARCHAR(20),
-    salary DECIMAL(10,2)
+    salary DECIMAL(10,2),
+    CONSTRAINT instructor_pk PRIMARY KEY (inst_id)
 );
 
-CREATE TABLE Courses(
-    course_id INT(10) PRIMARY KEY,
+CREATE TABLE Courses (
+    course_id INT(10) NOT NULL,
     course_name VARCHAR(50) NOT NULL,
     units INT(2) NOT NULL,
     instructor_id INT,
     schedule DATETIME,
     class_id INT,
-    FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id)
+    CONSTRAINT courses_pk PRIMARY KEY (course_id),
+    CONSTRAINT courses_fk_instructor FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id)
 );
 
 CREATE TABLE Classes (
-    class_id INT PRIMARY KEY,
+    class_id INT NOT NULL,
     course_id INT NOT NULL,
     program_id INT,
     instructor_id INT,
-    term VARCHAR(10), 
+    term VARCHAR(10),
     school_year YEAR,
     schedule DATETIME,
     room_number VARCHAR(20),
     max_capacity INT,
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    FOREIGN KEY (program_id) REFERENCES Programs(program_id),
-    FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id)
+    CONSTRAINT classes_pk PRIMARY KEY (class_id),
+    CONSTRAINT classes_fk_course FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+    CONSTRAINT classes_fk_program FOREIGN KEY (program_id) REFERENCES Programs(program_id),
+    CONSTRAINT classes_fk_instructor FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id)
 );
 
 ALTER TABLE Courses
 ADD COLUMN prerequisite_id INT,
-ADD FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id);
+ADD CONSTRAINT courses_fk_prerequisite FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id);
 
 CREATE TABLE Prerequisites (
     course_id INT(10),
     prerequisite_id INT(10),
-    PRIMARY KEY (course_id, prerequisite_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id)
+    CONSTRAINT prerequisites_pk PRIMARY KEY (course_id, prerequisite_id),
+    CONSTRAINT prerequisites_fk_course FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+    CONSTRAINT prerequisites_fk_prerequisite FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id)
 );
 
 CREATE TABLE Students (
-    student_id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR (50) NOT NULL,
-    last_name VARCHAR (50) NOT NULL,
+    student_id INT(11) AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     birth_date DATE,
-    gender VARCHAR (1),
+    gender CHAR(1),
     address VARCHAR(50),
     email VARCHAR(50),
-    phone_number VARCHAR(15),
     program_id INT,
-    FOREIGN KEY (program_id) REFERENCES Programs(program_id)
+    CONSTRAINT students_pk PRIMARY KEY (student_id),
+    CONSTRAINT students_fk_program FOREIGN KEY (program_id) REFERENCES Programs(program_id)
 );
+
+
