cee5d0a793554493f99ebca4eaa3319c85fd3b6d
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-11-10 16:03:21 +0800

    Update synch.sql

--- synch.sql
+++ synch.sql
@@ -1,8 +1,6 @@
-USE dbapp;
-
 CREATE TABLE Colleges (
     college_id INT NOT NULL,
-    college_name VARCHAR(50) NOT NULL,
+    college_name VARCHAR(100) NOT NULL,
     CONSTRAINT colleges_pk PRIMARY KEY (college_id),
     CONSTRAINT college_name_unique UNIQUE (college_name)
 );
@@ -18,17 +16,13 @@ VALUES
     (7, 'Ramon V. Del Rosario College of Business'),
     (8, 'School of Economics');
 
-
 CREATE TABLE Programs (
     program_id VARCHAR(20) NOT NULL,
-    program_name VARCHAR(100) NOT NULL,
+    program_name VARCHAR(300) NOT NULL,
     college_id INT NOT NULL,
     CONSTRAINT programs_pk PRIMARY KEY (program_id),
     CONSTRAINT programs_fk_college FOREIGN KEY (college_id) REFERENCES Colleges(college_id)
 );
-
-ALTER TABLE Programs
-MODIFY COLUMN program_name VARCHAR(300);
 
 INSERT INTO Programs (program_id, program_name, college_id)
 VALUES
@@ -96,14 +90,14 @@ VALUES
     ('SOE-BS-AEI', 'Bachelor of Science in Applied Economics major in Industrial Economics', 8),
     ('SOE-BS-AEF', 'Bachelor of Science in Applied Economics major in Financial Economics', 8),
     ('SOE-AB-ECM', 'Bachelor of Arts major in Economics', 8),
-     ('BAGCED-CEP', 'Counseling and Educational Psychology', 1),
+	('BAGCED-CEP', 'Counseling and Educational Psychology', 1),
     ('BAGCED-ELM', 'Educational Leadership and Management', 1),
     ('BAGCED-EAL', 'English and Applied Linguistics', 1),
     ('BAGCED-PE', 'Physical Education', 1),
     ('BAGCED-SE', 'Science Education', 1),
     ('BAGCED-OEP', 'Offshore and Extension Programs', 1),
     ('BAGCED-CIESOL', 'Center for Innovative Education Solutions (CIESOL)', 1);
-    
+
 CREATE TABLE Instructor (
     inst_id INT NOT NULL,
     inst_first_name VARCHAR(50) NOT NULL,
@@ -116,27 +110,103 @@ CREATE TABLE Instructor (
 );
 
 CREATE TABLE Courses (
-    course_id INT NOT NULL,
-    course_name VARCHAR(50) NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    course_name VARCHAR(100) NOT NULL,
     units INT(2) NOT NULL,
     instructor_id INT,
-    prerequisite_id INT,
+    prerequisite_id VARCHAR(10),
+    college_id INT NOT NULL,
     CONSTRAINT courses_pk PRIMARY KEY (course_id),
     CONSTRAINT courses_fk_instructor FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id),
-    CONSTRAINT courses_fk_prerequisite FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id)
+    CONSTRAINT courses_fk_prerequisite FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id),
+    CONSTRAINT courses_fk_college FOREIGN KEY (college_id) REFERENCES Colleges(college_id)
 );
 
-ALTER TABLE Courses
-ADD COLUMN college_id INT;
-
-ALTER TABLE Courses
-ADD CONSTRAINT courses_fk_college FOREIGN KEY (college_id) REFERENCES Colleges(college_id);
+-- Values for Computer Science -- 
+INSERT INTO Courses (course_id, course_name, units, college_id)
+VALUES
+    ('BASMATH', 'Algebra and Trigonometry', 5, 2),
+    ('BASSTAT', 'Algebra and Statistics', 3, 2),
+    ('CCICOMP', 'Introduction to Computing', 3, 2),
+    ('CCPROG1', 'Logic Formulation and Introductory Programming', 3, 2),
+    ('CCPROG2', 'Programming with Structured Data Types', 3, 2),
+    ('CCPROG3', 'Object-Oriented Programming', 3, 2),
+    ('CCDSTRU', 'Discrete Structures', 3, 2),
+    ('CCDSALG', 'Data Structures and Algorithms', 3, 2),
+    ('CCINFOM', 'Information Management', 3, 2),
+    ('CCAPDEV', 'Web Application Development', 3, 2),
+    ('CCINOV8', 'Innovation and Technology Management', 3, 2),
+    ('CSMATH1', 'Differential Calculus', 3, 2),
+    ('CSMATH2', 'Linear Algebra for Computer Science', 3, 2),
+    ('CSMODEL', 'Modelling and Simulation', 3, 2),
+    ('CSADPRG', 'Advanced Programming Techniques', 3, 2),
+    ('CSSWENG', 'Software Engineering', 3, 2),
+    ('CSALGCM', 'Algorithms and Complexity', 3, 2),
+    ('CSNETWK', 'Introduction to Computer Networks', 3, 2),
+    ('CSARCH1', 'Introduction to Computer Organization and Architecture 1', 3, 2),
+    ('CSARCH2', 'Introduction to Computer Organization and Architecture 2 (Lecture and Laboratory)', 4, 2),
+    ('CSOPESY', 'Introduction to Operating Systems', 3, 2),
+    ('CSINTSY', 'Introduction to Artificial Intelligence', 3, 2),
+    ('CSSECDV', 'Secure Web Development', 3, 2),
+    ('ST-MATH', 'Integral Calculus for Computer Science Students', 3, 2),
+    ('STALGC', 'Advanced Algorithms and Complexities', 3, 2),
+    ('MOBDEVE', 'Mobile Development', 3, 2),
+    ('STSWENG', 'Advanced Software Engineering', 3, 2),
+    ('STADVDB', 'Advanced Database Systems', 3, 2),
+    ('STHCUIX', 'Human Computer Interactions', 3, 2),
+    ('STINTSY', 'Advanced Intelligent Systems', 3, 2),
+    ('STDISCM', 'Distributed Computing', 3, 2),
+    ('STMETRE', 'Software Technology Research Methods', 3, 2),
+    ('NSCOM01', 'Network Application Protocols', 3, 2),
+    ('NSCOM02', 'Network Connectivity and Data Delivery', 3, 2),
+    ('NSCOM03', 'Data Communications', 3, 2),
+    ('NSSECU1', 'Introduction to Security', 3, 2),
+    ('NSSECU2', 'Advanced and Offensive Security', 3, 2),
+    ('NSSECU3', 'Defensive Security', 3, 2),
+    ('NSETRON', 'Practical Electricity and Electronics', 3, 2),
+    ('NSEMBED', 'Embedded Platform Development', 3, 2),
+    ('NSAPDEV', 'Server Application Development', 3, 2),
+    ('NSDSYST', 'Introduction to Distributed Systems', 3, 2),
+    ('NSRESME', 'Network and Information Security Research Methods', 3, 2),
+    ('CE-MATH', 'Mathematical Methods for Computer Systems Engineering', 3, 2),
+    ('CEETRN1', 'Fundamentals of Electric Circuits', 4, 2),
+    ('CEETRN2', 'Basic Principles of Electronic Circuits', 4, 2),
+    ('CEETRN3', 'Linear Circuits and Communication', 4, 2),
+    ('CEEDSGN', 'Advanced Topics in Electronic Circuits and Systems', 3, 2),
+    ('CEDISP1', 'Digital Signal Processing 1', 4, 2),
+    ('CEDISP2', 'Digital Signal Processing 2', 3, 2),
+    ('CEMICRO', 'Microprocessor Interfacing', 4, 2),
+    ('CECNTRL', 'Introduction to Microcontrollers', 3, 2),
+    ('CEPARCO', 'Multiprocessing and Parallel Computing', 3, 2),
+    ('CERESME', 'Computer Systems Engineering Research Methods', 3, 2),
+    ('ITISORG', 'Organization and Management Concepts', 3, 2),
+    ('ISBISPR', 'Organizations and Business Processes', 3, 2),
+    ('ISFINMA', 'Financial and Managerial Accounting for IS', 3, 2),
+    ('ISOPRES', 'Introduction to Operations Research', 3, 2),
+    ('ISINFOM', 'Transaction Management & Descriptive Analytics', 3, 2),
+    ('ITISHCI', 'Human-Computer Interaction', 3, 2),
+    ('ISBUSPE', 'Business Performance', 3, 2),
+    ('ITISDEV', 'Business Applications Development', 3, 2),
+    ('ISANDE1', 'Systems Analysis', 3, 2),
+    ('ISINFRA', 'Introduction to IT Infrastructures', 3, 2),
+    ('ISANDE2', 'Systems Design & Development', 3, 2),
+    ('ISSYSAD', 'Systems Administration', 3, 2),
+    ('ISRESME', 'IS Research Methods', 3, 2),
+    ('ITISSES', 'Special Enterprise Systems', 3, 2),
+    ('ISSYSQA', 'Systems Quality Assurance', 3, 2),
+    ('ISPRENL', 'Predictive Analytics', 3, 2),
+    ('ISENTAR', 'Enterprise Architecture', 3, 2),
+    ('ISSECUR', 'Information Security', 3, 2),
+    ('ISSTRMA', 'IT Strategic Management', 3, 2),
+    ('ITISPRJ', 'IT Project Management', 3, 2),
+    ('ISTREND', 'Trends in Information Systems', 3, 2);
 
 CREATE TABLE Classes (
     class_id INT NOT NULL,
-    course_id INT NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
     program_id VARCHAR(20),
     instructor_id INT,
+    college_id INT NOT NULL,
     term VARCHAR(10),
     school_year YEAR,
     schedule DATETIME,
@@ -145,19 +215,13 @@ CREATE TABLE Classes (
     CONSTRAINT classes_pk PRIMARY KEY (class_id),
     CONSTRAINT classes_fk_course FOREIGN KEY (course_id) REFERENCES Courses(course_id),
     CONSTRAINT classes_fk_program FOREIGN KEY (program_id) REFERENCES Programs(program_id),
-    CONSTRAINT classes_fk_instructor FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id)
+    CONSTRAINT classes_fk_instructor FOREIGN KEY (instructor_id) REFERENCES Instructor(inst_id),
+    CONSTRAINT classes_fk_college FOREIGN KEY (college_id) REFERENCES Colleges(college_id)
 );
 
-ALTER TABLE Classes
-ADD COLUMN college_id INT;
-
-ALTER TABLE Classes
-ADD CONSTRAINT classes_fk_college FOREIGN KEY (college_id) REFERENCES Colleges(college_id);
-
-
 CREATE TABLE Prerequisites (
-    course_id INT NOT NULL,
-    prerequisite_id INT NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    prerequisite_id VARCHAR(10) NOT NULL,
     CONSTRAINT prerequisites_pk PRIMARY KEY (course_id, prerequisite_id),
     CONSTRAINT prerequisites_fk_course FOREIGN KEY (course_id) REFERENCES Courses(course_id),
     CONSTRAINT prerequisites_fk_prerequisite FOREIGN KEY (prerequisite_id) REFERENCES Courses(course_id)
@@ -167,7 +231,7 @@ CREATE TABLE Students (
     student_id INT NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    birth_date DATE,
+    birth_date VARCHAR(15),
     gender CHAR(1),
     address VARCHAR(100),
     email VARCHAR(50) UNIQUE,
@@ -175,6 +239,3 @@ CREATE TABLE Students (
     CONSTRAINT students_pk PRIMARY KEY (student_id),
     CONSTRAINT students_fk_program FOREIGN KEY (program_id) REFERENCES Programs(program_id)
 );
-
-ALTER TABLE Students
-MODIFY COLUMN birth_date VARCHAR(15);
