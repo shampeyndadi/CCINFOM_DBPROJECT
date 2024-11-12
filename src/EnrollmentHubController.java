e078fdd72366952c54473aeb7bb7f13646d10e40
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-11-13 04:04:43 +0800

    Add files via upload

---
+++ src/EnrollmentHubController.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
+
+public class EnrollmentHubController {
+    private LoginView loginUI;
+    private StudentDao studentDao;
+
+    public EnrollmentHubController(){
+        this.loginUI = new LoginView();
+        this.studentDao = new StudentDao();
+
+        this.loginUI.setEnterButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    int idNumber = loginUI.getIdNumber();
+                    String password = loginUI.getPassword();
+
+                    if (studentDao.authenticateUser(idNumber, password)){
+                        StudentView studentView = new StudentView(studentDao, idNumber);
+                        new StudentViewController(studentView, loginUI, studentDao, idNumber);
+                        loginUI.resetTextFields();
+                        loginUI.dispose();
+                    }else{
+                        JOptionPane.showMessageDialog(loginUI, "Student not found.", "Login Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }catch(InputMismatchException e1){
+                    JOptionPane.showMessageDialog(loginUI, "Invalid ID format.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }catch (NumberFormatException e1 ){
+                    JOptionPane.showMessageDialog(loginUI, "Invalid Input.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+}
