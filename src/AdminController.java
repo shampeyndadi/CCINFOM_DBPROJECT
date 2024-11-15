8e9f3f8d01196989b969971bea8428541ca18335
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-11-15 19:00:59 +0800

    Update AdminController.java

--- src/AdminController.java
+++ src/AdminController.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class AdminController {
     private AdminView adminView;
@@ -13,8 +14,24 @@ public class AdminController {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AdminDao adminDao = new AdminDao();
+
                 adminView.openClassView();
                 adminView.populateComboBoxes(adminDao);
+
+                adminView.setListOfColleges(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String selectedCollege = adminView.getSelectedCollege();
+
+                        if (selectedCollege != null && !selectedCollege.isEmpty()) {
+                            int collegeId = adminDao.getCollegeIdByName(selectedCollege);
+
+                            ArrayList<String> courses = adminDao.getCourses(collegeId);
+                            adminView.setCourses(courses);
+                        }
+                    }
+                });
+
             }
         });
 
